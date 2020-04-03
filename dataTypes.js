14a72620dc33b5d810ca48416b5cf88e4ddaa551
akazad1235 <akazad914@gmail.com>
2020-04-03 15:57:57 +0600

    datTypes & typeConversion

---
+++ dataTypes.js
@@ -0,0 +1,16 @@
+//there are two types of data type
+// 1. primitive 2 object
+
+var name = 'this is string'
+var numbers = 20
+var boolT = true
+var boolF = false
+var nulls = null
+
+console.log(null)
+console.log(numbers)
+console.log(boolT)
+console.log(boolF)
+ var un 
+ console.log(un)
+ 
---
+++ typeConversion.js
@@ -0,0 +1,16 @@
+//toString Method that means any datatyes to string 
+
+var num = 202+20
+console.log((202+7).toString())
+console.log(num.toString())
+
+var num1 = true
+var num1 = false
+console.log(num1.toString())
+
+//Number Method that means string to number convert
+
+var str1 = Number('2010')
+console.log(str1)
+
+//Converting Variables to Numbers
