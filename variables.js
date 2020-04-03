31e8a2ca6ecc1c2502f903d84d337b206d557a15
akazad1235 <akazad914@gmail.com>
2020-04-03 09:07:46 +0600

    Initial setup, hello world display & variable declaration declaration

---
+++ variables.js
@@ -0,0 +1,21 @@
+var helloString = "hello string"
+console.log(helloString)
+
+var a= 10;
+var b=20;
+var sum=a+b;
+console.log(sum)
+
+var x = '20'
+var y = 30
+var add = x+y
+console.log(typeof(add))  
+
+var x = '20'
+var y = 30
+var add = x*y
+console.log(typeof(add))  
+var x = '20'
+var y = 30
+var add = x+y
+console.log(typeof(add))  
